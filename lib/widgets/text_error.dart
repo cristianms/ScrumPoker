e91686220d0883156e526834be770b7054fbea69
Cristian de Mellos <cristian.ms@hotmail.com>
2023-05-22 21:32:43 -0300

    Ajustes nullsafety

--- lib/widgets/text_error.dart
+++ lib/widgets/text_error.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class TextError extends StatelessWidget {
   final String msg;
 
-  const TextError(this.msg, {super.key});
+  const TextError(this.msg, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
